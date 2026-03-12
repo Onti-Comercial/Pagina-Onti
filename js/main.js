9d12e6db3fc667dc2e145fd22628e890a9013a2c
Onti-Comercial <impresosomsa@hotmail.com>
2026-03-12 15:04:44 -0600

    optimización de js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,44 @@
 window.addEventListener("load", function() {
+  // Le damos al celular un respiro de 100ms antes de ejecutar los carruseles
+  setTimeout(function() {
+      var owl = $("#header-slider");
 
-  var owl = $("#header-slider");
+      if (owl.length) {
+        owl.owlCarousel({
+          singleItem : true,
+          navigation : true,
+          navigationText : ["<"," >"],
+          transitionStyle : "fade",
+          pagination: true,
+        });
+      }
 
-  owl.owlCarousel({
-    singleItem : true,
-    navigation : true,
-    navigationText : ["<"," >"],
-    transitionStyle : "fade",
-    pagination: true,
-  });
+      if ($("#Client_Logo").length) {
+        $("#Client_Logo").owlCarousel({
+          autoPlay : 5000,
+          items : 6,
+          responsiveClass:true,
+          responsive: { 0:{ items : 1 }, 480:{ items : 1 }, 768:{ items : 3 }, 1200:{ items: 3 } }
+        });
+      }
 
-  $("#Client_Logo").owlCarousel({
-    autoPlay : 5000,
-    items : 6,
-    responsiveClass:true,
-    responsive: {
-      0:{ items : 1 },
-      480:{ items : 1 },
-      768:{ items : 3 },
-      1200:{ items: 3 }
-    }
-  });
+      if ($("#blog-post").length) {
+        $("#blog-post").owlCarousel({
+          autoPlay : 5000,
+          items : 3,
+          responsiveClass:true,
+          responsive: { 0:{ items : 1 }, 480:{ items : 1 }, 768:{ items : 3 }, 1200:{ items: 3 } }
+        });
+      }
 
-  $("#blog-post").owlCarousel({
-    autoPlay : 5000,
-    items : 3,
-    responsiveClass:true,
-    responsive: {
-      0:{ items : 1 },
-      480:{ items : 1 },
-      768:{ items : 3 },
-      1200:{ items: 3 }
-    }
-  });
+      if ($('#mixed-items').length) {
+        $('#mixed-items').mixItUp({
+          animation: { animateResizeContainer: false }
+        });
+      }
 
-  if ($('#mixed-items').length) {
-    $('#mixed-items').mixItUp();
-  }
-
-  if (typeof WOW === "function") {
-    new WOW().init();
-  }
-
-
+      if (typeof WOW === "function") {
+        new WOW().init();
+      }
+  }, 100); // 100 milisegundos de retraso
 });
